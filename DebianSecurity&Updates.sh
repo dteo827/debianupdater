ad5b2ca89868faf038eddf1772482147cc5871a5
dteo827 <dteo827@gmail.com>
2014-03-07 00:30:43 -0500

    Update DebianSecurity&Updates.sh

--- DebianSecurity&Updates.sh
+++ DebianSecurity&Updates.sh
@@ -71,7 +71,7 @@ fi
 
 if [[ $answerGoogleDNS = y ]] ; then
 
-    echo deb 8.8.8.8 >> /etc/resolv.conf
+    echo nameserver 8.8.8.8 >> /etc/resolv.conf
 fi
 
 if [[ $answerWegettinghard = y]] ; then
